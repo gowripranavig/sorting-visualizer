9a5aaffc5bdc82213e3876dde278f64ba3cb3913
gowripranavig <gowripranavig@gmail.com>
2026-04-27 19:22:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -380,9 +380,6 @@ function prevStep() {
 document.getElementById("speed").addEventListener("input", (e) => {
     speed = 1050 - e.target.value;
 });
-document.getElementById("size").addEventListener("input", (e) => {
-    generateArray(e.target.value);
-});
 
 async function play() {
     isPlaying = true;
@@ -458,6 +455,17 @@ function showComplexity(type) {
 
 // run on load
 generateArray();
+
+function updateArraySize() {
+    let size = document.getElementById("sizeInput").value;
+
+    if (size < 5 || size > 100) {
+        alert("Please enter a size between 5 and 100");
+        return;
+    }
+
+    generateArray(parseInt(size));
+}
 
 function showDescription(type) {
     const desc = document.getElementById("description");
